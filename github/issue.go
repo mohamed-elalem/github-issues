0630b6688d9c2cc00ccd7be2c21c879a74735169
Mohamed Elalem <mohamed.el.alem.2017@gmail.com>
2019-06-24 11:07:56 +0200

    get the first 1000 result from github api

--- github/issue.go
+++ github/issue.go
@@ -6,6 +6,8 @@ import (
 	"net/http"
 )
 
+var allowedError = http.StatusUnprocessableEntity
+
 type Issue struct {
 	URL           string   `json:"html_url"`
 	State         string   `json:"state"`
@@ -14,28 +16,39 @@ type Issue struct {
 	UpdatedAt     string   `json:"updated_at"`
 	Labels        []*Label `json:"labels"`
 	RepositoryURL string   `json:"repository_url"`
+	Title         string   `json:"title"`
 }
 
 type Issues []*Issue
 
 func (i Issues) Do() (Issues, error) {
-	issuesURL := BuildURL()
-	res, err := http.Get(issuesURL)
-	if err != nil {
-		return nil, err
-	}
-
-	defer res.Body.Close()
-
 	resObject := Response{}
+	for i := 1; i <= 10; i++ {
+		issuesURL := BuildURL(i)
+		res, err := http.Get(issuesURL)
+		if err != nil {
+			return nil, err
+		}
 
-	if err := json.NewDecoder(res.Body).Decode(&resObject); err != nil {
-		return nil, fmt.Errorf("couldn't parse response for url %v: %v", issuesURL, err)
+		if res.StatusCode == allowedError {
+			break
+		}
+
+		defer res.Body.Close()
+
+		tmpResObject := Response{}
+
+		if err := json.NewDecoder(res.Body).Decode(&tmpResObject); err != nil {
+			return nil, fmt.Errorf("couldn't parse response for url %v: %v", issuesURL, err)
+		}
+
+		issues := resObject.Issues
+		issues = append(issues, tmpResObject.Issues...)
+		resObject.Issues = issues
 	}
-
 	return resObject.Issues, nil
 }
 
-func BuildURL() string {
-	return fmt.Sprintf(`%s?q=%s+state:open+label:"%s"`, BaseURL, issuesQuery, issueLabel)
+func BuildURL(page int) string {
+	return fmt.Sprintf(`%s?q=%s+state:open+label:"%s"&per_page=100&page=%d`, BaseURL, issuesQuery, issueLabel, page)
 }
